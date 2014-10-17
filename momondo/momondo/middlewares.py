f559059bd14d95142c7176fcf197510d23d05cbb
German Gomez-Herrero <german.gomezherrero@kasku.org>
2014-10-17 10:55:09 +0200

    EHN: Automatically renew Tor's exit point when detected

--- momondo/momondo/middlewares.py
+++ momondo/momondo/middlewares.py
@@ -1,4 +1,9 @@
 import random
+from scrapy.exceptions import NotConfigured
+from scrapy import log
+from scrapy.utils.response import response_status_message
+import sh
+import time
 
 
 class RandomUserAgentMiddleware(object):
@@ -30,3 +35,46 @@ class ProxyMiddleware(object):
 
     def process_request(self, request, spider):
         request.meta['proxy'] = self.crawler.settings.get('HTTP_PROXY')
+
+
+class TorRetryMiddleware(object):
+
+    def __init__(self, settings):
+        if not settings.getbool('RETRY_ENABLED'):
+            raise NotConfigured
+        self.max_retry_times = settings.getint('RETRY_TIMES')
+        self.retry_wait = settings.getint('RETRY_WAIT')
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(crawler.settings)
+
+    def process_response(self, request, response, spider):
+        if response.status != 200:
+            # Try renewing Tor's exit point and try again
+            from nose.tools import set_trace; set_trace()  # XXX BREAKPOINT
+            pidof = sh.pidof
+            kill = sh.kill
+            kill('-s', 'SIGHUP', pidof('tor').rstrip('\n'))
+            time.sleep(self.retry_wait)
+            reason = response_status_message(response.status)
+            return self._retry(request, reason, spider) or response
+
+        return response
+
+    def _retry(self, request, reason, spider):
+        retries = request.meta.get('retry_times', 0) + 1
+
+        if retries <= self.max_retry_times:
+            log.msg(format="Retrying %(request)s (failed %(retries)d times):"
+                    "%(reason)s", level=log.DEBUG, spider=spider,
+                    request=request, retries=retries, reason=reason)
+            retryreq = request.copy()
+            retryreq.meta['retry_times'] = retries
+            retryreq.dont_filter = True
+            return retryreq
+        else:
+            log.msg(format="Gave up retrying %(request)s (failed"
+                    "%(retries)d times): %(reason)s", level=log.DEBUG,
+                    spider=spider, request=request, retries=retries,
+                    reason=reason)
--- momondo/momondo/settings.py
+++ momondo/momondo/settings.py
@@ -22,7 +22,12 @@ NEWSPIDER_MODULE = 'momondo.spiders'
 COOKIES_ENABLED = False
 
 # Avoid hitting momondo too hard by having a delay between consecutive requests
-DOWNLOAD_DELAY = 15
+DOWNLOAD_DELAY = 3
+
+# Retry after renewing Tor's exit point
+RETRY_ENABLED = True
+RETRY_TIMES = 3
+RETRY_WAIT = 10     # in seconds
 
 # The user agent will be changed from a pool of valid user agents, this is just
 # the default user agent
@@ -36,6 +41,7 @@ HTTP_PROXY = 'http://127.0.0.1:8123'
 DOWNLOADER_MIDDLEWARES = {
     'momondo.middlewares.RandomUserAgentMiddleware': 400,
     'momondo.middlewares.ProxyMiddleware': 410,
+    'momondo.middlewares.TorRetryMiddleware': 420,
     'scrapy.contrib.downloadermiddleware.useragent.UserAgentMiddleware':
         None
     # Disable compression middleware, so the actual HTML pages are cached
